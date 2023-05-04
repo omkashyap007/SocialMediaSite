312630028233d16c022b1bec822358ee885a355f
omkashyap007 <omkashyapcric@gmail.com>
2023-05-04 14:42:31 +0530

    Initial Commit

--- testing.py
+++ testing.py
@@ -39,4 +39,71 @@ w = 0
 p = [1,2,3]
 c = [0,1,1]
 
-print(Solution.findMaximizedCapital(k, w, p, c))
+# print(Solution.findMaximizedCapital(k, w, p, c))
+a =  [[0]*5 for _ in range(10) ] 
+a[0][1] = 10
+a[0][0] = 23
+print(a)
+
+
+from typing import List
+
+class Solution:
+
+    def inside(self , i , j , grid) :
+        if i in range(len(grid)) and j in range(len(grid[0])) : 
+            return True
+        return False
+    
+    def bfs(self , i , j, grid , visited ) : 
+        if not self.inside(i , j , grid) : 
+            return -1
+        if visited[i][j] : 
+            return "vis"
+        if grid[i][j] == "N" :
+            visited[i][j] = True
+            return -1
+        if grid[i][j] == "H" : 
+            visited[i][j] = True
+            top = self.bfs(i-1 , j , grid , visited)
+            bottom = self.bfs(i+1 , j , grid , visited )
+            left = self.bfs(i , j-1 , grid , visited )
+            right = self.bfs(i , j +1 , grid , visited)
+            if top == bottom == left == right == -1 : 
+                return -1
+            else : 
+                possible_list = []
+                if top != -1 or top != "vis": 
+                    possible_list.append(top)
+                    
+                if bottom != -1 or bottom !=  "vis" : 
+                    possible_list.append(bottom)
+                    
+                if left != -1 or left != "vis" : 
+                    possible_list.append(left)
+                    
+                if right != -1 or right != "vis" : 
+                    possible_list.append(right)
+                
+                return 1 + min(possible_list)    
+        if grid[i][j] == "W": 
+            return 0
+            
+                
+    def chefAndWells(self, n : int, m : int, grid : List[List[str]]) -> List[List[int]]:
+        ans = [ [0]*m for _ in range(n)]
+        visited = [[False for _ in range(m)] for _ in range(n)]
+        for i in range(n) : 
+            for j in  range(m) : 
+                symbol = grid[i][j]
+                if symbol ==  "N" : 
+                    ans[i][j] = -1
+                    continue
+                if symbol == "W" : 
+                    continue
+                if symbol == "H" : 
+                    distance = 2* self.bfs(i , j , grid , visited)
+                    ans[i][j] = distance
+                    print(ans)
+        return ans
+        
